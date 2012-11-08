c6f0d232e6abdee5aca406fc5e8d5de8f67862a0
Mark Kolich <social@kolich.com>
2012-11-08 15:49:15 -0800

    updating deps to use kolich-common 0.0.3.2

--- project/Build.scala
+++ project/Build.scala
@@ -33,7 +33,7 @@ object Dependencies {
 
   // Internal dependencies
 
-  private val kolichCommon = "com.kolich" % "kolich-common" % "0.0.3.1" % "compile"
+  private val kolichCommon = "com.kolich" % "kolich-common" % "0.0.3.2" % "compile"
 
   // External dependencies
 
